faeb6331a30d3cdd56ea4dc720cfb201763f76be
SSM987 <sphrmsavi385@gmail.com>
2025-03-04 21:53:17 +0330

    1403/12/14

--- E609.java
+++ E609.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class E609 {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter a word:");
-        String word = s.nextLine();
+        String word = sc.nextLine();
         for(int i = word.length()-1;i>=0;i--)
             System.out.print(word.charAt(i));
     }
