edf1848254ef683ea8f79ab4551686113eb48bc1
SSM987 <sphrmsavi385@gmail.com>
2025-03-05 21:17:24 +0330

    1403/12/15

---
+++ E616.java
@@ -0,0 +1,9 @@
+public class E616 {
+    public static void main(String[] args) {
+        for(byte i=1;i<=10;i++) {
+            for (byte j = 1; j <= 10; j++)
+                System.out.print((i * j) + "\t");
+            System.out.println();
+        }
+    }
+}
